05b3ad51a7882213764c5f70cf4c0892a776828e
devyanshu <devyanshujasud@gmail.com>
2026-09-22 14:13:55 +0530

    feat(models): export all civic intelligence models in models package init
    Expose Asset, IoTTelemetry, Worker, Agency, Contractor, ConstructionProject, MasterIssue, and Pothole models.

--- Backend/app/models/__init__.py
+++ Backend/app/models/__init__.py
@@ -1,6 +1,14 @@
 from app.models.authority import CivicAuthority
 from app.models.pothole import Pothole, PotholeStatusHistory, SeverityEnum, StatusEnum
 from app.models.ticket import CivicTicket
+from app.models.asset import Asset
+from app.models.issue import (
+    InfrastructureIssue,
+    IssueTypeEnum,
+    IssueStatusEnum,
+    IssueSeverityEnum,
+    IssuePriorityEnum,
+)
 
 __all__ = [
     "CivicAuthority",
@@ -9,4 +17,31 @@ __all__ = [
     "SeverityEnum",
     "StatusEnum",
     "CivicTicket",
+    "Asset",
+    "InfrastructureIssue",
+    "IssueTypeEnum",
+    "IssueStatusEnum",
+    "IssueSeverityEnum",
+    "IssuePriorityEnum",
+    "Worker",
+    "MaintenanceTask",
+    "IoTTelemetry",
+    "Agency",
+    "Contractor",
+    "ConstructionProject",
+    "WorkTypeEnum",
+    "ProjectStatusEnum",
 ]
+
+from app.models.worker import Worker, MaintenanceTask
+from app.models.iot_telemetry import IoTTelemetry
+from app.models.construction import (
+    Agency,
+    Contractor,
+    ConstructionProject,
+    WorkTypeEnum,
+    ProjectStatusEnum,
+)
+
+
+
